eec29ccd148aeeb430e9984440ff243b578cba7d
zhenglei4 <zhenglei4@yusys.com.cn>
2022-07-26 17:54:51 +0800

    222

--- src/main/java/com/zhenglei/dynamicdatasource/DynamicDatasourceApplication.java
+++ src/main/java/com/zhenglei/dynamicdatasource/DynamicDatasourceApplication.java
@@ -11,6 +11,7 @@ public class DynamicDatasourceApplication {
 
     public static void main(String[] args) {
         System.err.println("第一次提交");
+        System.err.println("第二次提交");
         SpringApplication.run(DynamicDatasourceApplication.class, args);
     }
 
